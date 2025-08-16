5de800d0b17570a3e7bc3804abe2796294f0d40c
KamranPA <155093978+KamranPA@users.noreply.github.com>
2025-08-16 23:44:11 +0330

    Update detect_bos.py

--- utils/detect_bos.py
+++ utils/detect_bos.py
@@ -1,30 +1,28 @@
 # utils/detect_bos.py
-def detect_bos(high, low, lookback=100):
+def detect_bos(high, low, lookback=50):
+    """
+    تشخیص Break of Structure (BOS)
+    - BOS نزولی: قیمت زیر یک HL قبلی برود
+    - BOS صعودی: قیمت بالاتر از یک LH قبلی برود
+    """
     if len(high) < lookback + 5:
         return None
 
-    # تشخیص HH و LL
-    recent_highs = list(high[-lookback:])
     recent_lows = list(low[-lookback:])
+    recent_highs = list(high[-lookback:])
 
-    hh = max(recent_highs)
-    ll = min(recent_lows)
-
-    idx_hh = recent_highs.index(hh)
-    idx_ll = recent_lows.index(ll)
-
-    current_price = high[-1]
-
-    # BOS نزولی: قیمت زیر HL قبلی برود
+    # BOS نزولی: شکست HL
     for i in range(-5, -2):
-        if low[i] > low[i-1] and low[i] > low[i+1]:  # HL
-            if current_price < low[i]:
+        if i >= -len(recent_lows): continue
+        if recent_lows[i] > recent_lows[i-1] and recent_lows[i] > recent_lows[i+1]:  # HL
+            if high[-1] < recent_lows[i]:
                 return 'bearish'
 
-    # BOS صعودی: قیمت بالاتر از HH قبلی برود
+    # BOS صعودی: شکست LH
     for i in range(-5, -2):
-        if high[i] < high[i-1] and high[i] < high[i+1]:  # LH
-            if current_price > high[i]:
+        if i >= -len(recent_highs): continue
+        if recent_highs[i] < recent_highs[i-1] and recent_highs[i] < recent_highs[i+1]:  # LH
+            if low[-1] > recent_highs[i]:
                 return 'bullish'
 
     return None
