727e5bb117678e99ca810f6d2963ce4072487fa6
KamranPA <155093978+KamranPA@users.noreply.github.com>
2025-08-17 13:42:24 +0330

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -4,25 +4,12 @@ from decouple import config
 
 def send_telegram_message(message):
     try:
-        # خواندن از .env یا محیط سیستم
         token = config('TELEGRAM_TOKEN')
         chat_id = config('TELEGRAM_CHAT_ID')
-
         url = f"https://api.telegram.org/bot{token}/sendMessage"
-        payload = {
-            "chat_id": chat_id,
-            "text": message,
-            "parse_mode": "HTML"
-        }
+        payload = {"chat_id": chat_id, "text": message, "parse_mode": "HTML"}
         response = requests.post(url, data=payload)
-
-        if response.status_code == 200:
-            print("✅ پیام به تلگرام ارسال شد.")
-            return True
-        else:
-            print(f"❌ خطا در ارسال تلگرام: {response.text}")
-            return False
-
+        return response.status_code == 200
     except Exception as e:
-        print(f"❌ خطای ارسال تلگرام: {e}")
+        print(f"❌ ارسال تلگرام شکست خورد: {e}")
         return False
