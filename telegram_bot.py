32fe1044b588008f0efb8bf74a7aa3771e00d4b8
KamranPA <155093978+KamranPA@users.noreply.github.com>
2025-08-17 13:01:49 +0330

    Create telegram_bot.py

---
+++ telegram_bot.py
@@ -0,0 +1,15 @@
+# telegram_bot.py
+import requests
+from config import TELEGRAM_TOKEN, TELEGRAM_CHAT_ID
+
+def send_telegram_message(message):
+    url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage"
+    payload = {
+        "chat_id": TELEGRAM_CHAT_ID,
+        "text": message,
+        "parse_mode": "HTML"
+    }
+    try:
+        requests.post(url, data=payload)
+    except Exception as e:
+        print(f"خطا در ارسال پیام: {e}")
