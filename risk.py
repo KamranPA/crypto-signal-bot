e47be5b406fab97051ca462fac8d9fd589c87b62
KamranPA <155093978+KamranPA@users.noreply.github.com>
2025-08-18 13:52:18 +0330

    Create risk.py

---
+++ risk.py
@@ -0,0 +1,12 @@
+def dynamic_stop_loss(entry_price, atr, direction="long"):
+    sl = entry_price - (1.5 * atr) if direction == "long" else entry_price + (1.5 * atr)
+    tp = entry_price + (2 * 1.5 * atr) if direction == "long" else entry_price - (2 * 1.5 * atr)
+    return sl, tp
+
+def position_size(capital, risk_percent=0.01, stop_loss_distance=None, price=None):
+    if stop_loss_distance is None or stop_loss_distance == 0:
+        return capital * 0.05  # 5% سرمایه
+    risk_per_unit = stop_loss_distance
+    risk_amount = capital * risk_percent
+    size = risk_amount / risk_per_unit
+    return size
