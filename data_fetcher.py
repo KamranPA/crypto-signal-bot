f1dbf0d7f495f2277b30f399db4e0992ba3e538d
KamranPA <155093978+KamranPA@users.noreply.github.com>
2025-08-18 21:06:25 +0330

    Update data_fetcher.py

--- data_fetcher.py
+++ data_fetcher.py
@@ -3,9 +3,12 @@ import pandas as pd
 from datetime import datetime
 
 def fetch_kucoin(symbol, timeframe="15min", start_date=None, end_date=None):
-    # تبدیل تاریخ به timestamp (ثانیه)
     def date_to_ts(date_str):
-        return int(datetime.strptime(date_str, "%Y-%m-%d").timestamp())
+        try:
+            return int(datetime.strptime(date_str, "%Y-%m-%d").timestamp())
+        except Exception as e:
+            print(f'❌ فرمت تاریخ نامعتبر: {date_str}')
+            return None
 
     url = "https://api.kucoin.com/api/v1/market/candles"
     params = {
@@ -14,16 +17,20 @@ def fetch_kucoin(symbol, timeframe="15min", start_date=None, end_date=None):
     }
 
     if start_date:
-        params["startAt"] = date_to_ts(start_date)
+        ts = date_to_ts(start_date)
+        if ts:
+            params["startAt"] = ts
     if end_date:
-        params["endAt"] = date_to_ts(end_date)
+        ts = date_to_ts(end_date)
+        if ts:
+            params["endAt"] = ts
 
     try:
         response = requests.get(url, params=params, timeout=10)
         data = response.json()
         if data["code"] == "200000":
             if not data["data"]:
-                print(f"⚠️ داده‌ای برای {symbol} در این بازه یافت نشد.")
+                print(f'⚠️ داده‌ای برای {symbol} در بازه زمانی یافت نشد.')
                 return None
 
             df = pd.DataFrame(data["data"],
@@ -35,8 +42,8 @@ def fetch_kucoin(symbol, timeframe="15min", start_date=None, end_date=None):
             df.sort_index(inplace=True)
             return df
         else:
-            print(f"❌ خطای KuCoin: {data['msg']}")
+            print(f'❌ خطای KuCoin: {data["msg"]}')
             return None
     except Exception as e:
-        print(f"❌ خطا در دریافت داده: {e}")
+        print(f'❌ خطا در دریافت داده: {e}')
         return None
