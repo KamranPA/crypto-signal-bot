fd3496884b4ea0d0fc697d7c11bdcf9066bacf54
KamranPA <155093978+KamranPA@users.noreply.github.com>
2025-08-22 14:25:51 +0330

    Update main.py

--- main.py
+++ main.py
@@ -101,7 +101,7 @@ def fetch_binance_testnet_ohlcv(symbol, timeframe, since_ms, until_ms):
             print(f"❌ خطای شبکه: {e}")
             break
 
-    if not all_
+    if not all_daya:
         return None
 
     ohlcv = []
