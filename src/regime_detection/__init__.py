12bfed92de66953d9d570b95f4df9e7977f2bf45
KamranPA <155093978+KamranPA@users.noreply.github.com>
2025-08-27 19:44:45 +0330

    Update __init__.py

--- src/regime_detection/__init__.py
+++ src/regime_detection/__init__.py
@@ -1,6 +1,9 @@
 # src/regime_detection/__init__.py
+
 from .range_detector import is_range_regime
-from .trend_detector import is_trend_regime
 from .breakout_detector import is_breakout_regime
 
-__all__ = ['is_range_regime', 'is_trend_regime', 'is_breakout_regime']
+__all__ = [
+    'is_range_regime',
+    'is_breakout_regime'
+]
