9ef4b268ceb71b0810444d3f81259b7b50cd37cd
KamranPA <155093978+KamranPA@users.noreply.github.com>
2025-08-27 19:16:00 +0330

    Update trading_system.py

--- src/strategy/trading_system.py
+++ src/strategy/trading_system.py
@@ -1,8 +1,8 @@
 # src/strategy/trading_system.py
 
-from trend_strategy import apply_trend_strategy
-from range_strategy import apply_range_strategy
-from breakout_strategy import apply_breakout_strategy
+from .trend_strategy import apply_trend_strategy
+from .range_strategy import apply_range_strategy
+from .breakout_strategy import apply_breakout_strategy
 
 def get_signal(df):
     if len(df) < 50:
